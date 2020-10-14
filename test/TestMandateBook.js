4cfeaa4521eb6424abf4a85e30d9d1b5894ece17
Ian Arden <ian@applicature.com>
2020-10-14 21:18:04 +0300

    deployment and test specs

--- test/TestMandateBook.js
+++ test/TestMandateBook.js
@@ -3,20 +3,37 @@ const MandateBook = artifacts.require('./MandateBook');
 contract('MandateBook', (accounts) => {
 
   const OWNER = accounts[0];
+  const INVESTOR1 = accounts[1];
+  const INVESTOR2 = accounts[2];
+  const MANAGER1 = accounts[3];
+  const MANAGER2 = accounts[4];
+  const OUTSIDER = accounts[5];
 
   let mandateBook;
 
   before('setup', async () => {
-    mandateBook = await MandateBook.new({from: OWNER})
+    mandateBook = await MandateBook.deployed();
   });
 
   afterEach('revert', async () => {
     //
   });
 
-  describe('status', async () => {
-    it('should doing smth', async () => {
+  describe('Investor Steps', async () => {
+    it('Investor should be able to create a Mandate', async () => {
       //
     });
+    it('Investor should be able to populate a Mandate');
+    it('Investor should be able to submit a Mandate to a Manager');
+    it('Manager should be able to accept the Mandate appointed by the Investor');
+    it('Manager should be able to decline the Mandate appointed by the Investor');
+    it('Manager should be able to start the Mandate that they accepted before');
+    it('Manager should be able to start the Mandate submitted to them by Investor');
+    it('Manager should be able to close in profit Mandate');
+    it('Manager should be able to close through the stoploss');
+    it('Investor should be able to close through the stoploss');
+    it('Manager should be able to settle the deal distributing the earnings');
+    it('Investor should be able to settle the deal distributing the earnings');
+
   });
 })
