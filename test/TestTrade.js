038343825a45b7fc2918b235ec81421b31043eb4
piron <paul.bolhar@gmail.com>
2020-10-16 23:57:38 +0300

    add TestTrade

--- test/TestTrade.js
+++ test/TestTrade.js
@@ -14,6 +14,12 @@ const {
   expectRevert,
 } = require("@openzeppelin/test-helpers");
 
+const {
+  takeSnapshot,
+  revertToSnapshot,
+  timeTravelTo
+} = require("./helper");
+
 function toWei(x) {
   return web3.utils.toWei(toBN(x) /*, "nano"*/);
 }
@@ -21,27 +27,38 @@ function toWei(x) {
 contract('Trade', ([OWNER, MINTER, INVESTOR1, INVESTOR2, MANAGER1, MANAGER2, OUTSIDER]) => {
   
   let trade;
+  let WETH, DAI, TKNX;
 
-  before('setup', async () => {
-    this.weth = await MockERC20.new('WETH', 'WETH', '100000000', { from: MINTER });
-    this.dai = await MockERC20.new('DAI', 'DAI', '100000000', { from: MINTER });
-    this.tokenX = await MockERC20.new('TokenX', 'TKNX', '100000000', { from: MINTER });
-    trade = await Trade.new({ from: OWNER });
+  beforeEach(async() => {
+    this.snapshotId = await takeSnapshot();
   });
 
   afterEach('revert', async () => {
-    //
+    await revertToSnapshot(this.snapshotId);
+  });
+
+  before('setup', async () => {
+    WETH = await MockERC20.new('WETH', 'WETH', '100000000', { from: MINTER });
+    DAI = await MockERC20.new('DAI', 'DAI', '100000000', { from: MINTER });
+    TKNX = await MockERC20.new('TokenX', 'TKNX', '100000000000', { from: MINTER });
+    trade = await Trade.new({ from: OWNER });
+  });
+
+  describe('Lock traders if investor getting big loss', async () => {
+    it('Should be possible to lock trading by agreement', async () => {})
+    it('Should not be possible to any trade if agreement is locked', async () => {})
   });
 
   describe('Swap ERC20 token to ERC20 token', async () => {
-    //
+    it('Should be possible to create new exchange', async () => {})
+    it('Should be possible to create 2 new exchanges', async () => {})
   });
 
   describe('Swap ETH to ERC20 token', async () => {
-    //
+    it('Should be possible to create new exchange', async () => {})
   });
 
   describe('Swap ERC20 token to ETH', async () => {
-    //
+    it('Should be possible to create new exchange', async () => {})
   });
 })
